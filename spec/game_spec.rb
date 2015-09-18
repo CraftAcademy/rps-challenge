aee6eb47892fa0f76589f9f73fd46cab5cc44faa
Christopher Comella <git.chrisco@gmail.com>
2015-09-18 17:50:39 +0200

    Write and pass rest of the  Game specs

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,11 +6,43 @@ describe Game do
       allow(Game).to receive(:computer_move).and_return("Rock")
     end
 
-    describe "The computer loses the game" do
+    describe "The player wins the game" do
       it "if the player picks 'Rock' and the computer picks 'Scissors'" do
         allow(Game).to receive(:computer_move).and_return("Scissors")
         result = Game.play("Rock")
         expect(result).to eq("You won!")
+      end
+
+      it "if the player picks 'Paper' and the computer picks 'Rock'" do
+        allow(Game).to receive(:computer_move).and_return("Rock")
+        result = Game.play("Paper")
+        expect(result).to eq("You won!")
+      end
+
+      it "if the player picks 'Scissors' and the computer picks 'Paper'" do
+        allow(Game).to receive(:computer_move).and_return("Paper")
+        result = Game.play("Scissors")
+        expect(result).to eq("You won!")
+      end
+    end
+
+    describe "The player loses the game" do
+      it "if the player picks 'Scissors' and the computer picks 'Rock'" do
+        allow(Game).to receive(:computer_move).and_return("Rock")
+        result = Game.play("Scissors")
+        expect(result).to eq("Computer won.")
+      end
+
+      it "if the player picks 'Rock' and the computer picks 'Paper'" do
+        allow(Game).to receive(:computer_move).and_return("Paper")
+        result = Game.play("Rock")
+        expect(result).to eq("Computer won.")
+      end
+
+      it "if the player picks 'Paper' and the computer picks 'Scissors'" do
+        allow(Game).to receive(:computer_move).and_return("Scissors")
+        result = Game.play("Paper")
+        expect(result).to eq("Computer won.")
       end
     end
 
