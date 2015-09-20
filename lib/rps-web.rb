0d0104a32c38c6e2b4bd4ffda0be2c47df3f29db
Bo Mattsson <bo.mattsson@cint.com>
2015-09-20 15:42:42 +0200

    Started with the game-page

--- lib/rps-web.rb
+++ lib/rps-web.rb
@@ -11,35 +11,11 @@ class RPSWeb < Sinatra::Base
 	end
 
 
-
-#class Game
-
-	#def initialize(player, computer)
+	post '/game' do
 		
-	#	@player = player.new
-	#	@computer = computer.new
-	
-	#end
+		erb :game
+	end
 
-		
-	#def self.computer_move
-   	#	%w(Rock Paper Scissors).sample
-  	#end
-
-  	#def self.play(player_move)
-   	#	computer_move
-
-	#	case move1
-	#	when :paper
-
-	#	if player_move == computer_move 
-	#		"Draw !"
-
-	#	elsif ((player_move == "Paper" && computer_move == "Stone") || (player_move) == "Scissors" && computer_move == "Paper" || (player_move) == "Stone" && computer_move == "Scissors"))
-	#		"You win !"
-	#	else
-	#		"Sorry, you lose"
-	#	end
 
 
 	run! if app_file == $0
