f67c0bf13fa709a4c37de4ced5ea8d00061fe64f
David <dhesabian@gmail.com>
2015-09-21 10:34:30 +0200

    first commit

---
+++ lib/game.rb
@@ -0,0 +1,46 @@
+class Game
+
+  attr_reader :players
+
+  def initialize(player, computer)
+    @player = player
+    @computer = computer
+    @players = [player, computer]
+    @beats = {
+      "rock" => ["paper"],
+      "paper" => ["scissors"],
+      "scissors" => ["rock"], 
+    }
+  end
+
+  def player
+    @player
+  end
+
+  def computer
+    @computer
+  end
+
+  def winner
+    return "Draw! Try again" if @player.pick == @computer.pick
+    return "Darn it. You lose." if @beats[@player.pick].include? @computer.pick 
+    return "You win!" if @beats[computer.pick].include? @player.pick
+  end
+end
+
+
+
+
+
+
+
+
+
+
+	#def winning_moves 
+    	#{"rock" => "scissors", "scissors" => "paper", "paper" => "rock"}
+  #end
+
+#I want to define winning moves
+#want it to understand when you win, lose, draw.
+#Moves
