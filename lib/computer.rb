143231ecc76487e83bb1fd141002ec2429f557a3
David <dhesabian@gmail.com>
2015-10-08 15:43:46 +0200

    test are passing, next try to see if it actually works and eventually put it up on the web?

--- lib/computer.rb
+++ lib/computer.rb
@@ -7,20 +7,3 @@ class Computer
   end
 end
 
-
-
-
-#class Computer 
-
-  #def name
-    #"Computer"
-  #end
-
-  #def move
-    #["rock", "paper", "scissors"].sample
-  #end
-
-#end
-
-#want computer to have a name (in this case computer)
-#Want computer to make a move
